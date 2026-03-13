26e44d8b49b54f19a8e27c425e17d7dc07c02045
Bekhruz Norov <105427207+BSNorov@users.noreply.github.com>
2026-03-13 16:58:22 +0500

    Create ruffier1.py

---
+++ ruffier1.py
@@ -0,0 +1,54 @@
+txt_index = "Ваш индекс Руфье: "
+txt_workheart = "Работоспособность сердца: "
+txt_nodata = "нет данных для такого возраста"
+
+txt_res = [
+"низкая. Срочно обратитесь к врачу!",
+"удовлетворительная. Обратитесь к врачу!",
+"средняя. Возможно, стоит дополнительно обследоваться у врача.",
+"выше среднего",
+"высокая"
+]
+
+
+def ruffier_index(P1, P2, P3):
+    return (4 * (P1 + P2 + P3) - 200) / 10
+
+
+def neud_level(age):
+    norm_age = (min(age, 15) - 7) // 2
+    result = 21 - norm_age * 1.5
+    return result
+
+
+def ruffier_result(r_index, level):
+
+    if r_index >= level:
+        return 0
+
+    level = level - 4
+    if r_index >= level:
+        return 1
+
+    level = level - 5
+    if r_index >= level:
+        return 2
+
+    level = level - 5.5
+    if r_index >= level:
+        return 3
+
+    return 4
+
+
+def test(P1, P2, P3, age):
+
+    if age < 7:
+        return txt_index + "0\n" + txt_nodata
+
+    ruff_index = ruffier_index(P1, P2, P3)
+    result = txt_res[ruffier_result(ruff_index, neud_level(age))]
+
+    res = txt_index + str(ruff_index) + '\n' + txt_workheart + result
+
+    return res
