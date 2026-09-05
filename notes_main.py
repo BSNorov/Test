d1e9e58dd7fec26ac2f4c6e6f50f34cb6519468a
Bekhruz Norov <105427207+BSNorov@users.noreply.github.com>
2026-09-05 14:24:56 +0500

    Update notes_main.py

--- notes_main.py
+++ notes_main.py
@@ -1,139 +1,80 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QListWidget, QLineEdit, QTextEdit, QInputDialog, \
-    QHBoxLayout, QVBoxLayout
+# print("Привет! Я компьютер Борис. Моя любимая еда - 'видеокарты'. А по пятницам я боюсь калькуляторов.")
+# print(5 + 5)
+# print(5 - 5)
+# print(5 * 5)
+# print(5 / 5)
 
-app = QApplication([])
-notes = []
+# print(input("Как тебя зовут? "))
 
-'''Интерфейс приложения'''
-# параметры окна приложения
-notes_win = QWidget()
-notes_win.setWindowTitle('Умные заметки')
-notes_win.resize(900, 600)
+# name = input("Как тебя зовут? ")
+# print(name)
+# print("Привет! Я компьютер Борис. Моя любимая еда - 'видеокарты'. А по пятницам я боюсь калькуляторов.")
+# print(5 + 5)
+# print(5 - 5)
+# print(5 * 5)
+# print(5 / 5)
 
-# виджеты окна приложения
-list_notes = QListWidget()
-list_notes_label = QLabel('Список заметок')
+# print(input("Как тебя зовут? "))
 
-button_note_create = QPushButton('Создать заметку')  # появляется окно с полем "Введите имя заметки"
-button_note_del = QPushButton('Удалить заметку')
-button_note_save = QPushButton('Сохранить заметку')
+# name = input("Как тебя зовут? ")
+# print(name)
 
-field_tag = QLineEdit('')
-field_tag.setPlaceholderText('Введите тег...')
-field_text = QTextEdit()
-button_tag_add = QPushButton('Добавить к заметке')
-button_tag_del = QPushButton('Открепить от заметки')
-button_tag_search = QPushButton('Искать заметки по тегу')
-list_tags = QListWidget()
-list_tags_label = QLabel('Список тегов')
+# name = input("Как тебя зовут? ")
+# food = input("Что ты любишь есть? ")
+#
+# print("Привет,", name)
+# print("Так и знал! Ты любишь", food)
 
-# расположение виджетов по лэйаутам
-layout_notes = QHBoxLayout()
-col_1 = QVBoxLayout()
-col_1.addWidget(field_text)
+# score = 10
+# print(score)
+#
+# score = 50
+# print(score)
 
-col_2 = QVBoxLayout()
-col_2.addWidget(list_notes_label)
-col_2.addWidget(list_notes)
-row_1 = QHBoxLayout()
-row_1.addWidget(button_note_create)
-row_1.addWidget(button_note_del)
-row_2 = QHBoxLayout()
-row_2.addWidget(button_note_save)
-col_2.addLayout(row_1)
-col_2.addLayout(row_2)
-
-col_2.addWidget(list_tags_label)
-col_2.addWidget(list_tags)
-col_2.addWidget(field_tag)
-row_3 = QHBoxLayout()
-row_3.addWidget(button_tag_add)
-row_3.addWidget(button_tag_del)
-row_4 = QHBoxLayout()
-row_4.addWidget(button_tag_search)
-
-col_2.addLayout(row_3)
-col_2.addLayout(row_4)
-
-layout_notes.addLayout(col_1, stretch=2)
-layout_notes.addLayout(col_2, stretch=1)
-notes_win.setLayout(layout_notes)
-
-'''Функционал приложения'''
+# age = int(input("Введите возраст: "))
+# print(age + 1)
 
 
-def show_note():
-    key = list_notes.selectedItems()[0].text()
-    print(key)
-    for note in notes:
-        if note[0] == key:
-            field_text.setText(note[1])
-            list_tags.clear()
-            list_tags.addItems(note[2])
+# weight = float(input("Введите ваш вес: "))
+# print(weight)
 
 
-def add_note():
-    note_name, ok = QInputDialog.getText(notes_win, "Добавить заметку", "Название заметки: ")
-    if ok and note_name != "":
-        note = list()
-        note = [note_name, '', []]
-        notes.append(note)
-        list_notes.addItem(note[0])
-        list_tags.addItems(note[2])
-        print(notes)
-        with open(str(len(notes) - 1) + ".txt", "w") as file:
-            file.write(note[0] + '\n')
+# print(3 // 2)
+
+# print(3 ** 4)
+
+rost = int(input("Введите ваш рост в см: "))
+metr = rost / 100
+mm = rost * 10
+print("Ваш рост в метрах:", metr)
+print("Ваш рост в миллиметрах:", mm)
+
+# name = input("Как тебя зовут? ")
+# food = input("Что ты любишь есть? ")
+#
+# print("Привет,", name)
+# print("Так и знал! Ты любишь", food)
+
+# score = 10
+# print(score)
+#
+# score = 50
+# print(score)
+
+# age = int(input("Введите возраст: "))
+# print(age + 1)
 
 
-def save_note():
-    if list_notes.selectedItems():
-        key = list_notes.selectedItems()[0].text()
-        index = 0
-        for note in notes:
-            if note[0] == key:
-                note[1] = field_text.toPlainText()
-                with open(str(index) + ".txt", "w") as file:
-                    file.write(note[0] + '\n')
-                    file.write(note[1] + '\n')
-                    for tag in note[2]:
-                        file.write(tag + ' ')
-                    file.write('\n')
-            index += 1
-        print(notes)
-    else:
-        print("Заметка для сохранения не выбрана!")
+# weight = float(input("Введите ваш вес: "))
+# print(weight)
 
 
-# обработка событий
-list_notes.itemClicked.connect(show_note)
-button_note_create.clicked.connect(add_note)
-button_note_save.clicked.connect(save_note)
+# print(3 // 2)
 
-# запуск приложения
-notes_win.show()
+# print(3 ** 4)
 
-name = 0
-note = []
-while True:
-    filename = str(name) + ".txt"
-    try:
-        with open(filename, "r") as file:
-            for line in file:
-                line = line.replace('\n', '')
-                note.append(line)
-        tags = note[2].split(' ')
-        note[2] = tags
-
-        notes.append(note)
-        note = []
-        name += 1
-
-
-    except IOError:
-        break
-
-print(notes)
-for note in notes:
-    list_notes.addItem(note[0])
-
-app.exec_()
+rost = int(input("Введите ваш рост в см: "))
+metr = rost / 100
+mm = rost * 10
+print("Ваш рост в метрах:", metr)
+print("Ваш рост в миллиметрах:", mm)
