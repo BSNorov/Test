2a5aa8a52c6e5de39ce391c9c8d89d2b5ff19a35
Bekhruz Norov <105427207+BSNorov@users.noreply.github.com>
2026-09-19 15:09:45 +0500

    Update notes_main.py

--- notes_main.py
+++ notes_main.py
@@ -1,64 +1,81 @@
-import pickle
-from mcpi.minecraft import Minecraft
+"""Булевый тип bool -> True и False --- логический тип данных"""
 
-mc = Minecraft.create()
+# print(5 > 10)
+# print(7 == 7)
 
+'''
+Операторы сравнения:
 
-def sort(n1, n2):
-    if n1 > n2:
-        return n2, n1
-    else:
-        return n1, n2
-
-
-def copy_structure(x1, y1, z1, x2, y2, z2):
-    # Расставляем координаты от меньшей к большей
-    x1, x2 = sort(x1, x2)
-    y1, y2 = sort(y1, y2)
-    z1, z2 = sort(z1, z2)
-
-    # Определяем размеры конструкции
-    width = x2 - x1
-    height = y2 - y1
-    length = z2 - z1
-
-    # Создаём список для хранения конструкции
-    structure = []
-
-    print("Пожалуйста, подождите...")
-
-    # Проходим по всем блокам конструкции
-    for column in range(height):
-        structure.append([])
-
-        for row in range(width):
-            structure[column].append([])
-
-            for depth in range(length):
-                block = mc.getBlockWithData(
-                    x1 + row,
-                    y1 + column,
-                    z1 + depth
-                )
-
-                structure[column][row].append(block)
-
-    return structure
+> - Больше
+< - Меньше
+!= - Не равно
+== - Равно
+>= - Больше или равно
+<= - Меньше или равно
 
 
-# Получаем первый угол конструкции
-input("Пройдите к первому углу и нажмите Enter в этом окне")
-x1, y1, z1 = mc.player.getTilePos()
+'''
 
-# Получаем противоположный угол конструкции
-input("Пройдите к противоположному углу и нажмите Enter в этом окне")
-x2, y2, z2 = mc.player.getTilePos()
 
-# Копируем конструкцию
-print("Копируем в файл")
-structure = copy_structure(x1, y1, z1, x2, y2, z2)
+"""Услонвый оператор"""
 
-# Сохраняем конструкцию в файл
-file = open("structure.txt", "wb")
-pickle.dump(structure, file)
-file.close()
+# name = input("Имя: ")
+# print("Привет,", name)
+# print("Добро пожаловать!")
+
+# age = int(input("Сколько тебе лет? "))
+
+# if age >= 18:
+#     print("Вход разрешён!")
+#     print("Добро пожаловать")
+#
+# print("Конец программы!")
+
+
+# age = int(input("Возраст: "))
+#
+# if age >= 18:
+#     print("✅Можно войти")
+# else:
+#     print("❌Вход запрещён")
+
+# password = input("Введите пароль: ")
+#
+# if password == "python123":
+#     print("🔓Доступ разрешён")
+# else:
+#     print("🔓Неверный пароль")
+
+
+# score = int(input("Сколько баллов ты получил? "))
+#
+# if score >= 90:
+#     print("Отлично")
+# elif score >= 70:
+#     print("Хорошо")
+# elif score >= 50:
+#     print("Нормально")
+# else:
+#     print("Нужно попробовать еще раз!")
+#
+
+
+"""Логические операторы - and, or, not"""
+
+
+# age = int(input("Возраст: "))
+# has_ticket = input("Есть билет? да/нет: ")
+#
+# if age >= 18 and has_ticket == 'да':
+#     print("Проходите")
+# else:
+#     print("❌Вход запрещён")
+
+
+
+age = 6
+
+if age < 7 or age >= 60:
+    print("Проез бесплатный")
+else:
+    print("Нужно купить билет")
